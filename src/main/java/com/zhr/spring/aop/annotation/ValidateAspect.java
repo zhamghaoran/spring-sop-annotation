a4110e923837919b622fee6b93d1623e62767d37
zhanghaoran <2017929959@qq.com>
2022-08-16 13:08:05 +0800

    ""

---
+++ src/main/java/com/zhr/spring/aop/annotation/ValidateAspect.java
@@ -0,0 +1,19 @@
+package com.zhr.spring.aop.annotation;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(1) // 切面的优先级
+public class ValidateAspect {
+//    @Pointcut("execution(* com.zhr.spring.aop.annotation.CalculatorImpl.*(..))")
+//    public void pointCut(){}
+    @Before("com.zhr.spring.aop.annotation.LoggerAspect.pointcut()")
+    public void beforeMethod() {
+        System.out.println("ValidateAspect --> 前置通知");
+    }
+}
